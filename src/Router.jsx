036acab331dfd33e508fae21245b73558f2d9843
daiane-dosanjos <dayanjos01@gmail.com>
2023-08-12 23:39:36 -0300

    rota

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,22 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/pages/Navbar";
+import Carrossel from "./components/pages/Carrossel";
 import QuemSomos from "./components/pages/QuemSomos";
 import ComoAjudar from "./components/pages/ComoAjudar";
 import Contato from "./components/pages/Contato";
 import Doacoes from "./components/pages/Doacoes";
 import Footer from "./components/pages/Footer";
-import Carrossel from "./components/pages/Carrossel";
+
 
 function Router() {
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
+      <Route path="/" element={<Carrossel />} />
         <Route path="/quemsomos" element={<QuemSomos />} />
         <Route path="/comoajudar" element={<ComoAjudar />} />
         <Route path="/contato" element={<Contato />} />
-        <Route path="/doacoes" element={<Doacoes />} />
-        <Route path="*" element={<Carrossel />} />
+        <Route path="/doacoes" element={<Doacoes />} />       
       </Routes>
       <Footer />
     </BrowserRouter>
